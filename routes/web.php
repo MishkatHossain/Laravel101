795bf51ade50aba7504eaa5e861bb2aad29c5adc
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-08-06 08:41:51 +0600

    justice

--- routes/web.php
+++ routes/web.php
@@ -2,41 +2,22 @@
 
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
-class Jobs {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Sales Manager',
-                'salary' => '$30,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Programmer',
-                'salary' => '$20,000',
-            ],
-        ];
-    }
-}
+
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => Jobs::all()]);  
+    return view('jobs', ['jobs' => Job::all()]);  
 });
 
 Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
 
-    $job = Arr::first(Jobs::all(), fn($job) => $job['id'] == $id);
+    
     return view('job', ['job' => $job]);
 });
 
