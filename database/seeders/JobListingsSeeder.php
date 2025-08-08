cb55ac13663a7709d65c262ac5566332d4b9db9d
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-08-08 06:15:22 +0600

    just practices soe db: seed --class=JobListingsSeeder

---
+++ database/seeders/JobListingsSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class JobListingsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('job_listings')->insert([
+            [
+                'title' => 'Laravel Developer',
+                'salary' => '50000',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Wordpress Developer',
+                'salary' => '40000',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Software Developer',
+                'salary' => '50000',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'title' => 'Software Engineer',
+                'salary' => '60000',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
