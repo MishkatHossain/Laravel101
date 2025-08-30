eadac5aef2a24a7013a119dc973817c6e8ef89fc
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-08-30 09:05:26 +0600

    x/mas/dark/

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Job;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test User',
+            'last_name' => 'test@example.com',
             'email' => 'test@example.com',
+            'password' => 'devOps123',
         ]);
+
+        $this->call(JobSeeder::class);
+
+
+
     }
 }
