cbb2b512bea8855a3c5ac05096773a0b90d97eba
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-09-10 08:52:53 +0600

    ( . )

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -21,7 +21,10 @@
                         <div class="mt-2">
                             <div
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="title" type="text" value="{{$job->title}}" name="title"
+                                <input id="title" 
+                                    type="text" 
+                                    value="{{$job->title}}" 
+                                    name="title"
                                     placeholder="Shift Leader"
                                     class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
                                     required />
@@ -39,8 +42,12 @@
                         <div class="mt-2">
                             <div
                                 class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="salary" type="number" name="salary" placeholder="$50,000 Per Year"
+                                <input id="salary" 
+                                    type="number" 
+                                    name="salary" 
+                                    placeholder="$50,000 Per Year"
                                     class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    value="{{$job->salary}}"
                                     required />
                             </div>
 
@@ -53,27 +60,15 @@
 
                 </div>
 
-                {{-- <div class='mt-10'>
-                    @if($errors->any())
-                    <ul>
-                        @foreach($errors->all() as $error)
-                        <li class='text-red-600 font-semibold italic'>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                    @endif
-                </div> --}}
-
             </div>
-
-
 
 
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
+            <a href="/jobs/{{$job->id}}" type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
             <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
         </div>
     </form>
 
