56059b7b6cdf1367d233d32b048ad6f156f3a1e3
Mishkat Hossain <hossainmishkat0@gmail.com>
2025-09-10 08:19:35 +0600

    xmen

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -19,12 +19,16 @@
                     <div class="sm:col-span-4">
                         <label for="title" class="block text-sm/6 font-medium text-gray-900">Edit Title</label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="title" type="text" value="{{$job->title}}" name="title" placeholder="Shift Leader" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
+                            <div
+                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input id="title" type="text" value="{{$job->title}}" name="title"
+                                    placeholder="Shift Leader"
+                                    class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    required />
                             </div>
 
                             @error('title')
-                                <p class='text-xs text-red-400 font-semibold mt-1'>{{ $message }}</p>
+                            <p class='text-xs text-red-400 font-semibold mt-1'>{{ $message }}</p>
                             @enderror
 
                         </div>
@@ -33,32 +37,31 @@
                     <div class="sm:col-span-4">
                         <label for="salary" class="block text-sm/6 font-medium text-gray-900">Edit Salary</label>
                         <div class="mt-2">
-                            <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                                <input id="salary" type="number" name="salary" placeholder="$50,000 Per Year" class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
+                            <div
+                                class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                                <input id="salary" type="number" name="salary" placeholder="$50,000 Per Year"
+                                    class="block min-w-0 grow bg-white py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    required />
                             </div>
 
                             @error('salary')
-                                <p class='text-red-400 font-semibold text-xs mt-1'>{{ $message }}</p>
+                            <p class='text-red-400 font-semibold text-xs mt-1'>{{ $message }}</p>
                             @enderror
                         </div>
                     </div>
 
 
                 </div>
-                
+
                 {{-- <div class='mt-10'>
-                    @if($errors->any()) 
-                        <ul>
-                            @foreach($errors->all() as $error)
-                                <li class='text-red-600 font-semibold italic'>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+                    @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                        <li class='text-red-600 font-semibold italic'>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                     @endif
                 </div> --}}
-
-
-
-
 
             </div>
 
@@ -69,7 +72,8 @@
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-            <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+            <button type="submit"
+                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
         </div>
     </form>
 
